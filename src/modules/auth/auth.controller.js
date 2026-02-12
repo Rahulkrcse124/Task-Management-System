fe6c4e21def99ae6105baf3c52090aa9f39b798d
Rahul Kumar <rahulkumarcse124@gmail.com>
2026-02-12 22:25:04 +0530

    Fixed pool import to default db

--- src/modules/auth/auth.controller.js
+++ src/modules/auth/auth.controller.js
@@ -1,4 +1,4 @@
-import { pool } from "../../config/db.js";
+import db from "../../config/db.js";
 import { createOtp } from "./auth.service.js";
 import { compareHash } from "../../utils/hash.js";
 import {
@@ -10,16 +10,18 @@ import { logActivity } from "../activity/activity.service.js";
 
 export const requestOtp = async (req, res) => {
   const { email } = req.body;
+
   const otp = await createOtp(email);
   await sendEmail(email, otp);
   await logActivity(null, "OTP_REQUEST", req);
+
   res.json({ message: "OTP sent successfully" });
 };
 
 export const verifyOtp = async (req, res) => {
   const { email, otp } = req.body;
 
-  const { rows } = await pool.query(
+  const { rows } = await db.query(
     "SELECT * FROM otps WHERE email=$1 ORDER BY created_at DESC LIMIT 1",
     [email],
   );
@@ -27,13 +29,14 @@ export const verifyOtp = async (req, res) => {
   if (!rows.length) return res.status(400).json({ message: "Invalid OTP" });
 
   const valid = await compareHash(otp, rows[0].otp_hash);
+
   if (!valid || new Date(rows[0].expires_at) < new Date())
     return res.status(400).json({ message: "Invalid or expired OTP" });
 
-  let user = await pool.query("SELECT * FROM users WHERE email=$1", [email]);
+  let user = await db.query("SELECT * FROM users WHERE email=$1", [email]);
 
   if (!user.rows.length) {
-    user = await pool.query("INSERT INTO users(email) VALUES($1) RETURNING *", [
+    user = await db.query("INSERT INTO users(email) VALUES($1) RETURNING *", [
       email,
     ]);
   }
@@ -41,7 +44,7 @@ export const verifyOtp = async (req, res) => {
   const accessToken = generateAccessToken(user.rows[0].id);
   const refreshToken = generateRefreshToken(user.rows[0].id);
 
-  await pool.query("UPDATE users SET refresh_token=$1 WHERE id=$2", [
+  await db.query("UPDATE users SET refresh_token=$1 WHERE id=$2", [
     refreshToken,
     user.rows[0].id,
   ]);
