e84145bf20e2a5fac9c3c592f32cf9d1030a3f99
Rahul Kumar <rahulkumarcse124@gmail.com>
2026-02-12 22:17:31 +0530

    Fixed db.js default export

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,15 @@
 import pkg from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 const { Pool } = pkg;
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
+pool.on("connect", () => {
+  console.log("PostgreSQL Connected");
+});
+
+export default pool;
